6d3f628e1e4602b1e5223f053809037812685291
Raj <rajtukadiya265@gmail.com>
2024-10-02 16:59:57 +0530

    add GrtProductByCategory , Update and Delete Product Minimal API

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+	//public record DeleteProductRequest(Guid Id);
+
+	public record DeleteProductResponse(bool IsSuccess);
+	public class DeleteProductEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapDelete("/products/{id}" , async (Guid id, ISender sender) =>
+			{
+				var result = await sender.Send(new DeleteProductCommand(id));
+
+				var response = result.Adapt<DeleteProductResponse>();
+
+				return Results.Ok(response);
+			})
+			.WithName("DeletProduct")
+			.Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Delet Product")
+			.WithDescription("Delet Product");
+		}
+	}
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -10,7 +10,7 @@ namespace Catalog.API.Products.GetProductById
 		public void AddRoutes(IEndpointRouteBuilder app)
 		{
 			app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
-			{
+			{ 
 				var result = await sender.Send(new GetProductByIdQuery(id));
 				
 				var response = result.Adapt<GetProductByIdResponse>();
