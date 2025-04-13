c490735bdbcee607e831c57d0c6dac705118cbaa
Raj <rajtukadiya265@gmail.com>
2025-04-13 14:55:37 +0530

    Crete End-To-End Basket Service with redis distributed cache support and also add in docker microservices and add checkHealth Point.

---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Exceptions;
+
+public class BasketNotFoundException : NotFoundException
+{
+    public BasketNotFoundException(string username): base("Basket", username)
+    {
+        
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,51 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssembly(assembly);
+	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+	config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
+});
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+	opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+	opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration = builder.Configuration.GetConnectionString("Redis");
+	//options.InstanceName = "Basket";
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+	.AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+	.AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+	new HealthCheckOptions
+	{
+		ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+	});
+
+app.Run();
